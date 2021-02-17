7c7510a964606b368cc7be5d28242983298f4d69
LexBranon <lbranon@csumb.edu>
2021-02-16 19:56:57 -0800

    lab6

---
+++ lab6/js/lab.js
@@ -0,0 +1,25 @@
+// lab6.js - Arrays and Objects
+// Author: Lexi Branon
+// Created: February 16, 2021
+// License: Public Domain
+
+
+console.log("Arrays and Objects")
+
+
+// Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004
+var make = "Chevy";
+var model = "Spark";
+var year = 2019;
+var color = "grey";
+var extras = "small and cute";
+
+// Use document.writeln() to neatly output each of your variables, example, document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Extras: " + extras + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
