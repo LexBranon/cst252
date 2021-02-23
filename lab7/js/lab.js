ae9f3a2e5d7988970b25ca13d2e82dd84df65b92
LexBranon <lbranon@csumb.edu>
2021-02-23 15:04:22 -0800

    lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,13 +3,54 @@
 // Created: February 23, 2021
 // License: Public Domain
 
-// declare a variable userName and use window.prompt() to get the user's name from the user
 function nameEntry() {
-  var userName = window.prompt("Hi. What's your name?");
-  document.writeIn("<style>:root {--textlen: " + userName.length + ";}</style>");
-  var sortedName = sortUserName (userName);
-  document.writeIn("Your name sorted:" + sortedName + "</br></br>");
-  var randomName = randomizeName(userName);
-    document.writeln("Your name randomized:<br>Do you like your new name?");
-    document.writeln("<div class='name'>" + randomName + "</div>"")
+    var userName = window.prompt("Hi. Nice to meet you! What's your name?");
+    document.writeln("You said your name was: " + userName + "</br>");
+    document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
+    var sortedName = sortUserName(userName);
+    document.writeln("Let's mix it up: " + sortedName + "</br></br>");
+    var randomName = randomizeName(userName);
+    document.writeln("Your new name is: <br>");
+    document.writeln("<div class='name'>" + randomName + "</div>")
+}
+
+nameEntry();
+
+  function shuffleArray(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+
+      while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+
+
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+      }
+      return array;
+  }
+
+  function toTitleCase(str) {
+      return str.replace(
+          /\w\S*/g,
+          function(txt) {
+              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+          }
+      );
+  }
+
+  function sortUserName(userName) {
+      return userName.toLowerCase().split("").sort().join("");
+  }
+
+  function randomizeName(userName) {
+      var nameArray = userName.toLowerCase().split("");
+      console.log("nameArray =", nameArray);
+      var shuffledArray = shuffleArray(nameArray);
+      console.log("shuffledArray =", shuffledArray);
+      var shuffledString = shuffledArray.join("");
+      var newName = toTitleCase(shuffledString);
+      return newName;
   }
