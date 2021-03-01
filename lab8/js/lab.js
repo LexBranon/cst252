66437a193c0fbb5227185bf63650b2fc975eb95e
LexBranon <lbranon@csumb.edu>
2021-02-28 17:58:04 -0800

    lab8

---
+++ lab8/js/lab.js
@@ -0,0 +1,26 @@
+// lab7.js - Functions
+// Author: Lexi Branon
+// Created: February 23, 2021
+// License: Public Domain
+
+array
+(4) [2, 5, 10, 100]
+function sqr(x) {
+    return
+}
+
+sqr (2)
+undefined
+function sqr(x) {
+    return x**2
+}
+
+sqr (2)
+4
+
+a.map (sqr); [2,5,10,100]
+
+array.map(sqr);
+(4) [4, 25, 100, 10000]
+
+console.log ("sqr", result);
