66299f7913b9e1ee751288b4f1000b7e0087c8b7
LexBranon <lbranon@csumb.edu>
2021-04-06 16:41:08 -0700

    lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,20 +1,47 @@
 // lab.js - Functions
 // Author: Lexi Branon
-// Created: March 23, 2021
+// Created: April, 2021
 // License: Public Domain
 
-function Person(first, last, age) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.school = "Hogwarts";
+function Vehicle(make, model, year, color, extras) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.extras = extras;
+    this.info = function() {
+      return(this.make + " " + this.model + " " + this.year + " " + this.color + " " + this.extras);
+    }
 }
-Person.prototype.language = "English";
-Person.prototype.name = function() {
-        return(this.firstName + " " + this.lastName);
-};
 
+var vehicles =[];
+
+var car = new Vehicle("Chevy", "Spark", 2019, "grey", "small and cute");
 var bike = new Vehicle("Schwinn", "LeToure", 1974, "gold", "Brooks saddle");
 
+
 // we add text (including <p> tags to the end of our output div
-document.getElementById("output").innerHTML += "<p>" + bike.info() + "</p>";
+document.getElementById("output").innerHTML += "<p>" + bike.info() + "</p>" + "<p>" + car.info() + "</p>";
+
+
+// Controls the game play
+        //class hangman {
+          // initialize
+        //  constructor(name1, name2) {
+              // set up the board
+            //  this.board = new Board();
+              // set up the players
+            //  this.player1 = new Player(name1, 1, this.board);
+            //  this.player2 = new Player(name2, 2, this.board);
+              // assign the starting player
+            //  this.current_player = this.player1;
+        //  }
+          // start play method
+        //  play() {
+              // loop infinitely
+            //  while(true) {
+                  // call the board rendering method]
+                //  this.board.render();
+                  // ask for coordinates from the current player
+                //  current_player.get_coordinates();
+        // and so on...
