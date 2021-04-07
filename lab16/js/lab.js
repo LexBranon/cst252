daf3226671684c57893d4255f3f39f0a8f131796
LexBranon <lbranon@csumb.edu>
2021-04-06 21:51:07 -0700

    lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,20 +1,10 @@
 // lab.js - Functions
 // Author: Lexi Branon
-// Created: March 23, 2021
+// Created: April, 2021
 // License: Public Domain
 
-function Person(first, last, age) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.school = "Hogwarts";
-}
-Person.prototype.language = "English";
-Person.prototype.name = function() {
-        return(this.firstName + " " + this.lastName);
-};
-
-var bike = new Vehicle("Schwinn", "LeToure", 1974, "gold", "Brooks saddle");
-
-// we add text (including <p> tags to the end of our output div
-document.getElementById("output").innerHTML += "<p>" + bike.info() + "</p>";
+$(document).ready(function(){
+  $("p").click(function(){
+    alert("Hey, you know how to read and click! Look at you! Impressive stuff man!");
+  });
+});
