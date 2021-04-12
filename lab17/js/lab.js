97680ba8641db29a5d2e702503d34ecc3e114cec
LexBranon <lbranon@csumb.edu>
2021-04-11 21:38:52 -0700

    lab17

---
+++ lab17/js/lab.js
@@ -0,0 +1,10 @@
+// lab.js - Functions
+// Author: Lexi Branon
+// Created: April, 2021
+// License: Public Domain
+
+//Add a click event to each button
+//In the event code, toggle a new class within the div
+$("#challenge-button").click(function(){
+ $("challenge-button").toggleClass("switch")
+})
